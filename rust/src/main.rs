abe8792f97b96c9331d8bd28b0d5a16a32c19ed9
lrth06 <lrth06@gmail.com>
2021-10-15 10:51:45 -0400

    add rust example

---
+++ rust/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+fn main() {
+    let args = std::env::args().nth(1).expect("No file name given!");   
+    println!("{:?}", args);
+
+    let mut data = String::new();
+    let f = File::open(args).expect("Unable to open file");
+    let mut br = BufReader::new(f);
+    br.read_to_string(&mut data).expect("Unable to read string");
+    println!("{}", data);
+
+}
